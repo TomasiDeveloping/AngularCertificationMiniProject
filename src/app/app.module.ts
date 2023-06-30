62daa0e492cc5b921c584b321c18372f2e4a0850
Tomasi - Developing <info@tomasi-developing.ch>
2023-06-30 19:49:59 +0200

    Github page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {AppRoutingModule} from "./app-routing.module";
-import {HttpClient, HttpClientModule} from "@angular/common/http";
+import { HttpClientModule} from "@angular/common/http";
 import {ReactiveFormsModule} from "@angular/forms";
 import { HomeComponent } from './pages/home/home.component';
 import { TrivialQuestionComponent } from './pages/trivial-question/trivial-question.component';
